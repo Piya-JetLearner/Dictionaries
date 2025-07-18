037fb53975d2883312d48a1fdc8830e3a416284f
Piya-JetLearner <piyajhp@gmail.com>
2025-07-18 18:33:20 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,17 +8,33 @@ my_dictionary={}
 # #del my_dictionary["orange"]
 # for item in my_dictionary:
 #     print(item,my_dictionary[item])
-print("\nWelcome to my online dictiopnary! ")
-print("1. Add/update a word")
-print("2. Retrieve a word's definition")
-print("3. Delete a word")
-print("4. View all the words")
-print("5.Exit")
-user=int(input("enter what number you want to do: "))
-if user==1:
-    word=input("What word do you want to use?")
-    meaning=input("What is the meaning of this word?")
-    my_dictionary[word]=meaning
-elif user==2:
-    word=input("enter a word you want to learn:")
-    print(my_dictionary[word])
+while True:
+    print("\nWelcome to my online dictiopnary! ")
+    print("1. Add/update a word")
+    print("2. Retrieve a word's definition")
+    print("3. Delete a word")
+    print("4. View all the words")
+    print("5.Exit")
+    user=int(input("enter what number you want to do: "))
+
+    if user==1:
+        word=input("What word do you want to use?")
+        meaning=input("What is the meaning of this word?")
+        my_dictionary[word]=meaning
+    elif user==2:
+        word=input("enter a word you want to learn:")
+        if word in my_dictionary:
+            print(my_dictionary[word])  
+        else:
+            print("this word is not in the dictionary.")   
+    elif user==3:
+        word=input("which word do you want to remove?")
+        if word in my_dictionary:
+            del my_dictionary[word]
+        else:
+            print("this word is not in the dictionary.")   
+    elif user==4:
+        for item in my_dictionary:
+            print(item,my_dictionary[item])
+    elif user==5:
+        break
